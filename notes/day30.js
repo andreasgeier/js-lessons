ae49b4bc56fa89adafb03ad4feca65b2fa418a71
Andreas Geier <938244+andreasgeier@users.noreply.github.com>
2023-06-23 12:13:02 +0200

    add day 30 with separated files for html, css, js

---
+++ notes/day30.js
@@ -0,0 +1,38 @@
+'use strict'
+
+{
+    // Hilfsfunktion $ und $$
+    const $ = q => document.querySelector(q)
+    const $$ = q => Array.from(document.querySelectorAll(q))
+
+    // Eventlistener registrieren
+    const init = () => $('#member_search input')
+        .addEventListener('keyup', 
+            event => {
+                console.log(event)
+                // updateHighlights($('#member_search input').value)
+                updateHighlights(event.target.value)
+            }
+    )
+
+    // Aktualisiere Highlighting
+    const updateHighlights = partOfMemberName => {
+        removeHighlights()
+        highlightNamesBy(partOfMemberName)
+    }
+
+    // Hervorhebe Teilnehmer mit bestimmten Buchstabenfolgen
+    const highlightNamesBy = partOfMemberName => {
+        if (partOfMemberName === '') return // 'guard' to check if input makes sense
+        $$('#chat_members li')
+        .filter(liNode => liNode.innerHTML.toLowerCase().includes(partOfMemberName))
+        .forEach(li => li.classList.add('highlighted'))
+    }
+
+    // Entferne alle Highlights
+    const removeHighlights = () =>
+        $$('#chat_members li')
+        .forEach(node => node.classList.remove('highlighted'))
+
+    init()
+}
